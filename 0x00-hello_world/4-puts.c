5551ccf3e85f141788bd0e9c173e853bd94c573e
ikettee <ikettee@gmail.com>
2023-09-07 14:21:27 -0700

    Task 4; Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *  main - C Program that Prints with puts function
+ *  Return: should always return o for success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle);
+	return(0);
+}
+
