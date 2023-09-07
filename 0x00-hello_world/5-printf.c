c5e93f472f271eff456042155cebe5278baad8e4
ikettee <ikettee@gmail.com>
2023-09-07 15:51:48 -0700

    Task 5; Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - C Program that prints with the printf function
+ * Return: The return value should be 0 for successful execution
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
