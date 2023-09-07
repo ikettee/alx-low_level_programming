ec9f2ee0eb021dee8407a494a6191815474812a0
ikettee <ikettee@gmail.com>
2023-09-07 16:57:01 -0700

    Task 6; Size is not a grandeur, and terrotory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,15 +5,9 @@
  */
 int main(void)
 {
-	char c;
-	int i;
-	long int l;
-	long long int d;
-	float f;
-
-	printf("Size of a char; %lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("Size of a int; %lu byte(s)\n", (unsigned long)sizeof(i));
-	printf("Size of a long int; %lu byte(s)\n", (unsigned long)sizeof(l));
-	printf("Size of a long long int; %lu byte(s)\n", (unsigned long)sizeof(d));
-	printf("Size of a float; %lu byte(s)\n", (unsigned long)sizeof(f));
+	printf("Size of a char: %lu byte(s)", sizeof(char));
+	printf("Size of a int: %lu byte(s)", sizeof(int));
+	printf("Size of a long int: %lu byte(s)", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)", sizeof(float));
 	return (0);
