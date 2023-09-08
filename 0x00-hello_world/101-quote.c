44c9e1c09406c08b1eb04d381c32ed5a884829fe
ikettee <ikettee@gmail.com>
2023-09-07 17:20:58 -0700

    Task 8; Unix is basically a simple operating system

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+# include <unistd.h>
+/**
+ * main - C Program that prints exactly and that piece of art is useful
+ * Return: value should be 1 for success
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
