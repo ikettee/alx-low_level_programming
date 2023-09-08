6a072430183b029bc34b8a39e6e478407739cc68
ikettee <ikettee@gmail.com>
2023-09-08 14:13:21 -0700

    Task 7; Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - the main function
+ * Return: should return 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'z' ; i >= 'a' ; i--)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
