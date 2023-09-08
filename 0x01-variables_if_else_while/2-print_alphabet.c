04309aee8e37c851e932c0246b5dcf713842b4dc
ikettee <ikettee@gmail.com>
2023-09-08 12:28:40 -0700

    Task 2; I sometimes suffer from insomnia. And when i can't fall asleep

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - the main function
+ * Return: should return 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
