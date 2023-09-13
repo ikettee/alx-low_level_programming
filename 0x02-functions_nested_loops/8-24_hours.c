32736de0c214dc033983c5b0b8d5b3b46b783426
ikettee <ikettee@gmail.com>
2023-09-12 22:46:35 -0700

    I'm federal agent Jack Bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int g, k;
+
+	for (g = 0 ; g < 24 ; g++)
+	{
+		for (k = 0 ; k < 60 ; k++)
+		{
+			_putchar(g / 10 + '0');
+			_putchar(g % 10 + '0');
+			_putchar(':');
+			_putchar(k / 10 + '0');
+			_putchar(k % 10 + '0');
+		}
+	}
+}
