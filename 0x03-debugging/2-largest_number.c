c88752df5572cac6c09e1fabd952d8f2e5f710b2
ikettee <ikettee@gmail.com>
2023-09-14 07:07:57 -0700

    task2; 0>972

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,28 +1,3 @@
-#include <stdio.h>
-#include "main.h"
-
-/**
-* main - prints the largest of 3 integers
-* Return: 0
-*/
-
-int main(void)
-{
-        int a, b, c;
-        int largest;
-
-        a = 972;
-        b = -98;
-        c = 0;
-
-        largest = largest_number(a, b, c);
-
-        printf("%d is the largest number\n", largest);
-
-        return (0);
-}
-carrie@ubuntu:/debugging$
-carrie@ubuntu:/debugging$ cat 2-largest_number.c
 #include "main.h"
 
 /**
@@ -35,20 +10,20 @@ carrie@ubuntu:/debugging$ cat 2-largest_number.c
 
 int largest_number(int a, int b, int c)
 {
-    int largest;
-
-    if (a >= b && b >= c)
-    {
-        largest = a;
-    }
-    else if (b >= a && a >= c)
-    {
-        largest = b;
-    }
-    else
-    {
-        largest = c;
-    }
-
-    return (largest);
+	int largest;
+	
+	if (a >= b && b >= c)
+	{
+		largest = a;
+	}
+	else if (b >= a && a >= c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
+	
+	return (largest);
 }
