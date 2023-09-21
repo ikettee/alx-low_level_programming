da240606dc03230710d695394dac992333fbc25e
ikettee <ikettee@gmail.com>
2023-09-21 00:52:48 -0700

    task 6; expect the best

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -18,14 +18,14 @@ char *cap_string(char *str)
 		str[index - 1] == '\n' ||
 		str[index - 1] == ',' ||
 		str[index - 1] == ';' ||
-		str[index - 1] == ',' ||
+		str[index - 1] == '.' ||
 		str[index - 1] == '!' ||
 		str[index - 1] == '?' ||
 		str[index - 1] == '"' ||
 		str[index - 1] == '(' ||
 		str[index - 1] == ')' ||
-		str[index - 1] == '(' ||
-		str[index - 1] == ')' ||
+		str[index - 1] == '{' ||
+		str[index - 1] == '}' ||
 		index == 0)
 			str[index] -= 32;
 		index++;
