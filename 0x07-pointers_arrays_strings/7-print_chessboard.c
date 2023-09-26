cb5115707b94b3150bf5ab5a02b20b3b0d539bd0
ikettee <ikettee@gmail.com>
2023-09-25 23:08:48 -0700

    task 6; chess is mental torture

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,7 +2,7 @@
 /**
  * print_chessboard - a function that prints the chessboard
  * @a: an array
- * Return: should be 0
+ * Return: should be 0 for success
  */
 void print_chessboard(char (*a)[8])
 {
@@ -11,7 +11,7 @@ void print_chessboard(char (*a)[8])
 
 	for (g = 0; g < 8; g++)
 	{
-		for (k 0; k < 8; k++)
+		for (k = 0; k < 8; k++)
 			_putchar(a[g][k]);
 		_putchar('\n');
 	}
