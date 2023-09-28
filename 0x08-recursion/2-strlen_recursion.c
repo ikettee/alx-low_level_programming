8011d8e6f75302d41b55afb217f5fa51a526f600
ikettee <ikettee@gmail.com>
2023-09-27 21:22:21 -0700

    task 2; dreams feel real while we're in them

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen_recursion - a function that returns the length of a string
+ * @s: string to measure
+ * Return: the length of string
+ */
+int _strlen_recursion(char *s)
+{
+	int length = 0;
+
+	if (*s)
+	{
+		length++;
+		length += _strlen_recursion(s + 1);
+	}
+
+	return (length);
+}
