c5591fdc29f56aad80997b6ac7aec08ed94c0a6e
ikettee <ikettee@gmail.com>
2023-09-29 22:40:36 -0700

    task 5; minimal number of coins for change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -22,13 +22,13 @@ int main(int argc, char **argv)
 	{
 		if (cent >= 25)
 			cent -= 25;
-		if (cent >= 10)
+		else if (cent >= 10)
 			cent -= 10;
-		if (cent >= 5)
+		else if (cent >= 5)
 			cent -= 5;
-		if (cent >= 2)
+		else if (cent >= 2)
 			cent -= 2;
-		if (cent >= 1)
+		else if (cent >= 1)
 			cent -= 1;
 		mincoin += 1;
 	}
