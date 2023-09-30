a709fc266e36f540ac6a0d52ae09caa13aedabc6
ikettee <ikettee@gmail.com>
2023-09-29 22:14:33 -0700

    task 4; to infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - a program that adds positive numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: should be 0
+ */
+int main(int argc, char *argv[])
+{
+	int g, k, add = 0;
+
+	for (g = 1; g < argc; g++)
+	{
+		for (k = 0; argv[g][g] != '\0'; k++)
+		{
+			if (!isdigit(argv[g][k]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		add += atoi(argv[g]);
+	}
+	printf("%d\n", add);
+	return (0);
+}
