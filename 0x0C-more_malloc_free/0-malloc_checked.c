78ac7e46fe83fe8dc99a78c74be6579ff1c2fd2a
ikettee <ikettee@gmail.com>
2023-10-05 01:55:49 -0700

    Task 0; Trust no one

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,15 +3,15 @@
 
 /**
  * *malloc_checked - a function that allocated memory using malloc
- * @size: the number of bytes allocated
+ * @b: the number of bytes allocated
  * Return: pointer to the allocated memory or exit with status 98
  */
 
-void *malloc_checked(unsigned int size)
+void *malloc_checked(unsigned int b)
 {
 	void *memory;
 
-	memory = malloc(size);
+	memory = malloc(b);
 
 	if (memory == NULL)
 		exit(98);
