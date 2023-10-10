3d0a7b3a61f6803aaf2c16bb332b48e41826291c
ikettee <ikettee@gmail.com>
2023-10-09 22:27:56 -0700

    task 0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a new type struct dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: its owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
