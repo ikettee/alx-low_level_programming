f172667a93ae939af96e8cfc78617007ac99f8b8
Isaac Gama <ikettee@gmail.com>
2023-10-19 20:13:55 -0700

    task 0; Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -5,11 +5,11 @@
 /**
  * print_list - a function that prints all the elements of a list_t list
  * @h: the list
- * Return: the number of nodes
+ * Return: the number of node
  */
-size_t print_list(const list_t *h);
+size_t print_list(const list_t *h)
 {
-	size_t nodes = 0;
+	size_t node = 0;
 
 	while (h)
 	{
@@ -18,9 +18,9 @@ size_t print_list(const list_t *h);
 		else
 			printf("[%d] %s\n", h->len, h->str);
 
-		nodes++;
+		node++;
 		h = h->next;
 	}
 
-	return (nodes);
+	return (node);
 }
