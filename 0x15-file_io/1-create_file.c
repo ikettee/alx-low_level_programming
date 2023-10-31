55d1581824b2f5ed60feace90d2352a0a5493fdd
Isaac Gama <ikettee@gmail.com>
2023-10-30 20:44:16 -0700

    1. Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,7 +26,7 @@ int create_file(const char *filename, char *text_content)
 	for (nle = 0; text_content[nle]; nle++)
 		;
 
-	readwrd = write(fil, text_content, nle);
+	readwr = write(fil, text_content, nle);
 
 	if (readwr == -1)
 		return (-1);
