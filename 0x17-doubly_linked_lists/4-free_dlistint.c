d40b61c2c0708047ced998ba4f57694abbb561c7
Isaac Gama <ikettee@gmail.com>
2023-12-07 21:24:52 -0800

    0x17. C - Doubly linked lists

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -7,13 +7,15 @@
  **/
 void free_dlistint(dlistint_t *head)
 {
-	if (head == NULL)
-		return;
+	dlistint_t *tmp;
 
-	while (head->next)
+	if (head != NULL)
+		while (head->prev != NULL)
+			head = head->prev;
+
+	while ((tmp = head) != NULL)
 	{
 		head = head->next;
-		free(head->prev);
+		free(head);
 	}
-	free(head);
 }
